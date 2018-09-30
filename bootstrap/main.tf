ebd9d481770d8c54afc8a2feae86fe2d7646b0af
saturnism <saturnism@gmail.com>
2018-09-30 17:39:16 -0400

    initial bootstrap

---
+++ bootstrap/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  project     = "${var.admin_project}"
+}
+
+resource "google_folder" "admin" {
+  display_name = "IT Administration Projects"
+  parent     = "organizations/${var.org_id}"
+}
+
+resource "google_project" "infrastructure" {
+  name = "Infrastructure as Code Project"
+  project_id = "${var.terraform_backend_project}"
+  folder_id  = "${google_folder.admin.name}"
+  billing_account = "${var.billing_account}"
+}
+
+resource "google_storage_bucket" "tf-gcs-store" {
+  name = "${var.terraform_backend_bucket}"
+  location = "US"
+  project = "${google_project.infrastructure.project_id}"
+  storage_class = "MULTI_REGIONAL"
+  versioning = {
+    enabled = true
+  }
+}
---
+++ bootstrap/variables.tf
@@ -0,0 +1,19 @@
+variable "org_id" {
+  type = "string"
+}
+
+variable "admin_project" {
+  type = "string"
+}
+
+variable "billing_account" {
+  type = "string"
+}
+
+variable "terraform_backend_project" {
+  type = "string"
+}
+
+variable "terraform_backend_bucket" {
+  type = "string"
+}
